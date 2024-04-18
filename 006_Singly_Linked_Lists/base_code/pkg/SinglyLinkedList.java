196a2b0a33793c9b35b20d0fbdffa1ff4a1eb8a4
Jayson Song <jaysonsong@jaysons-mbp.gusd.lcl>
2024-04-18 14:32:28 -0700

    singlyLinkedList

--- 006_Singly_Linked_Lists/base_code/pkg/SinglyLinkedList.java
+++ 006_Singly_Linked_Lists/base_code/pkg/SinglyLinkedList.java
@@ -18,14 +18,50 @@ public class SinglyLinkedList {
 	   	If the position doesn't exist, it returns -1
 	*/ 
 	public int get(int pos){
+		Node current = head;
+		int cnt = 0;
+		boolean flag = false;
 
+
+		while(current != null ){
+			if(cnt == pos){
+				flag = true;
+				break;
+			}
+			else{
+				cnt++;
+				current = current.getNext();
+			}
+		}
+		if(flag){
+			return current.getData();
+		}else{
+			return -1;
+		}
+		
+		
+		
 	}
 
 	/*
 		Insert a new Node at the given position with the data given
 	*/
-	public void insert(int pos int data){
+	public void insert(int pos, int data){
+		int cnt = 0;
+		Node current = head;
+		Node temp = new Node(data);
 
+		if(current == null){
+			return;
+		}
+
+		while(current != null){
+			if(cnt == pos-1){
+				current.setNext(temp);
+			}else{
+				current = current.getNext();
+			}
+		}
 	}
 
 	/*
@@ -33,14 +69,33 @@ public class SinglyLinkedList {
 		If no position exists, don't change the list
 	*/
 	public void remove(int pos){
+		Node current = head;
+		Node current2 = head;
+	
+		int data = 0;
 
+		for(int i=0; i<=pos+1; i++){
+			if(i == pos+1){
+				data = current2.getData();
+			}
+			current2 = current2.getNext();
+		}
+
+		for(int i=0; i<pos; i++){
+			if(pos-1 == i){
+				current.setNext()
+			}
+		}
 	}
 
 	/*
 		Swap two Nodes with the two positions given
 	*/
 	public void swap(int pos1, int pos2){
-
+		Node current = head;
+		for(int i=0; i<pos1; i++){
+			
+		}
 	}
 
 	/*
